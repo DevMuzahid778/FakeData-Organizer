53d5253099a0a298d8d4e7c5409c559f955d561d
Mujahid <mdmuzahid.gd@gmail.com>
2023-11-24 22:24:30 +0600

    navigation is look like fancy

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const usersData = useLoaderData()
     return (
         <div>
-            This is Home
+            
         </div>
     );
 };
